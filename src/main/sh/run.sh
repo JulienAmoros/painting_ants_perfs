99fed146819874c7ebc1c7c171172f6685c5800f
Julien Amoros <ju.amoros@laposte.net>
2017-12-12 08:54:09 +0100

    launch auto worst case

--- src/main/sh/run.sh
+++ src/main/sh/run.sh
@@ -5,7 +5,7 @@ FOLDER="../html/";
 if [ $# -eq 1 ]; then
     FILE="$FOLDER/$1";
 else
-    FILE="$FOLDER/ants_default.html";
+    FILE="$FOLDER/ants_worst.html";
 fi
 
 mvn -f ../../../pom.xml clean install
